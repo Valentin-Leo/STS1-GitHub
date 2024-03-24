f704f2be75fb94bb91f5232c3cb23a04d1ee3144
LilianRiviere <lilianriviere020@gmail.com>
2024-03-24 15:53:47 +0100

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -16,6 +16,51 @@ using namespace std;
 
 
 //Fonctions réalisées par l'étudiant 2 :
+bool estPair(int nb)
+{
+    if (nb % 2 == 0 || nb % 7 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+int sommeNombre(int nb) 
+{
+    int somme = 0;
+    for (int i = 1; i <= nb; ++i) 
+    {
+        somme += i;
+    }
+    return somme;
+}
+
+
+float salaireNet(int salaire)
+{
+    salaire = salaire * 0.77;
+
+    salaire = salaire * 1.12;
+
+    return salaire;
+}
+
+
+int plusPetit(int a, int b, int c) {
+    int min = a; 
+
+
+    if (b < min) {
+        min = b;
+    }
+    if (c < min) {
+        min = c;
+    }
+    return min;
+}
+
+
 
 
 
